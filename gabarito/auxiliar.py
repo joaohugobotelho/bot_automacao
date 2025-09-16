78f2b5d44653e7c8e3215a32e6d282f75db5f48e
joaohugobotelho <joaohugo202301@gmail.com>
2025-09-16 16:31:27 -0300

    automação na parte do login finalizada

---
+++ gabarito/auxiliar.py
@@ -0,0 +1,14 @@
+# pagina auxiliar do codigo pra pegar a posição exata do ponteiro mouse
+
+import pyautogui
+import time
+
+time.sleep(5) 
+
+print(pyautogui.position())
+
+# Point(x=760, y=406) do email
+
+# Point(x=735, y=504) da senha
+
+# Point(x=966, y=567) do login
--- gabarito/main.py
+++ gabarito/main.py
@@ -1,5 +1,6 @@
 import pyautogui
-
+pyautogui.PAUSE = 0.5 # comando pra dizer ao pyautogui pra ele esperar 0.5 segundo pra executar cada linha de codigo 
+import time # biblioteca do py para fazer um sleep
 # pyautogui.click -> clickar em algum lugar
 # pyautogui.press -> apertar 1 tecla
 # pyautogui.write -> escreveer um texto
@@ -7,7 +8,33 @@ import pyautogui
 # passo 1: entrar no sistema da empresa = https://dlp.hashtagtreinamentos.com/python/intensivao/login
 pyautogui.press("win")
 pyautogui.write("chrome")
+#pyautogui.write("Internet")
+pyautogui.press("enter")
+
+# abrir o perfil correto do google(somente pra quem possui mais de um perfil)
+time.sleep(1)
+pyautogui.click(x=787, y=572)
+
+#digitar  o site
+
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+pyautogui.press("enter")
+
+# esperar 3 seg
+time.sleep(3)
 # passo 2: faer login
+# preenche o email
+pyautogui.click(x=760, y=406)
+pyautogui.write("teste@gmail.com")
+time.sleep(1)
+#preenche a senha
+pyautogui.click(x=735, y=504)
+pyautogui.write("senhaqualquer")
+pyautogui.press("enter")
+time.sleep(1)
+#clica no login
+pyautogui.click(x=966, y=567)
+
 # passo 3: importar base dados
 # passo 4: cadastrar 1 produto
 # passo 5: reoetir parar todos os outros produtos
