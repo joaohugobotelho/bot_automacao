089c452f02f11fc2b8698ea05baee0d3f2d3b2d2
joaohugobotelho <joaohugo202301@gmail.com>
2025-09-16 10:00:20 -0300

    iniciando automação

---
+++ gabarito/main.py
@@ -0,0 +1,15 @@
+import pyautogui
+
+# pyautogui.click -> clickar em algum lugar
+# pyautogui.press -> apertar 1 tecla
+# pyautogui.write -> escreveer um texto
+
+# passo 1: entrar no sistema da empresa = https://dlp.hashtagtreinamentos.com/python/intensivao/login
+pyautogui.press("win")
+pyautogui.write("chrome")
+# passo 2: faer login
+# passo 3: importar base dados
+# passo 4: cadastrar 1 produto
+# passo 5: reoetir parar todos os outros produtos
+
+# pyautogui -> faz automaçoes com py
